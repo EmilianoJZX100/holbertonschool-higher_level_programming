d3b8c84916f4fc533df68236debd55ea320d8110
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-09 14:07:53 -0700

    5-print_comb2.py fix first numbers

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(00, 10):
-    print("{:0>2d}".format(i), ",", end="")
-for i in range(11, 100):
-    print("{}".format(i), ",", end="")
+for i in range(00, 100):
+    if i < 10:
+        print("{:0>2d}".format(i), ",", end="")
+    else:
+        print("{}".format(i), ",", end="")
