91f6f7bc464d6fcae9b22d434e92bc4e289372e3
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-11 17:12:47 -0700

    9-print_last_digit.py

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        opossite = number * -1
+        print(f"{opposite % 10}", end="")
+        return opposite % 10
+    elif number > 0:
+        print(f"{number % 10}", end="")
+        return number % 10
+    else:
+        print(f"{number}",  end="")
+        return number
