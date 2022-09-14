8f49faaf8e0a0616a96c266cf75396ae2f5c681b
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-14 16:38:09 -0700

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    def vdir(hidden_4):
+    def dir(hidden_4):
         return [x for x in dir(hidden_4) if not x.startswith('__')]
     print(dir(hidden_4))
