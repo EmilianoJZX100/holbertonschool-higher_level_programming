0abae86e4c72e8ef0da60c5c881d9e39ada8ef39
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-14 15:24:45 -0700

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    quantity = len(sys.argv)
+    total = 0
+
+    for i in range(1, quantity):
+        total = total + int(sys.argv[i])
+
+    print(f"{total}")
