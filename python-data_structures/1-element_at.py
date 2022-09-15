cc5a7b66a4402fbd1b2e157f4d0ef6b826351f6d
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-15 11:57:21 -0700

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(0, len(my_list)):
-        if i < 0:
+        if idx < 0:
             return None
-        elif i > len(my_list):
+        elif idx > len(my_list):
             return None
         else:
             return idx
