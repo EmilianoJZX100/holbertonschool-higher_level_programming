862094da42741c024298e0af3edb237f72264da9
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-15 14:06:02 -0700

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx <= 0 and idx >= range(0, len(my_list)) and idx == len(my_list):
+    if idx <= 0 and idx >= range(0, len(my_list)):
         return my_list
-    my_list[idx] = element
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_list
