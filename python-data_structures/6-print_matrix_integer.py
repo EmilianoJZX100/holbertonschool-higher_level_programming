fd2c1775856e081e0ad8c5a9cc7c50add4427744
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 08:12:33 -0700

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in matrix[i]:
-            print("{:d} ".format(j), end='')
-        print('')
+    for i in range(len(matrix)):             #goes thru every sub-list in matrix
+        for j in matrix[i]:                  #j goes thru every element in that matrix sub-list
+            print("{:d} ".format(j), end='') #prints every element of the sub-list without going to a new line
+        print()                              #creates a new line after each sub-list prints over
