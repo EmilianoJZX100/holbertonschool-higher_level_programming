4852b500f230faa2ff646adcf892beea5230a1ab
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 16:26:12 -0700

    0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for i in range(len(matrix)):
+        for idx, j in enumerate(matrix[i]):
+            if idx + 1 != len(matrix[i]):
+                "{:d ** 2} ".format(j), end=''
+        return new_matrix
+
