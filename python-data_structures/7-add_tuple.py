d1dbf6e56cdc2425a0979f58cc8fe000528ec5e2
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 11:34:19 -0700

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple(map(sum, zip(tuple_a, tuple_b)))
-    return new_tuple
+    add_tuple(map(sum, zip(tuple_a, tuple_b)))
+    return add_tuple
