62bc0e63e4bc9775f0e1ad532c9aed03733fcd2a
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 14:21:50 -0700

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,6 +4,8 @@ def divisible_by_2(my_list=[]):
     for n in range(len(my_list)):
         if n % 2 == 0:
             new_list.append(True)
+        elif 1 % 2 == 0:
+            new_list.append(False)
         else:
             new_list.append(False)
         return new_list
