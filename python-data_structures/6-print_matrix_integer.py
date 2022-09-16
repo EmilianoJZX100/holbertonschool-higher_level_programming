f98d7e43eb6b25de8491e2be7123f5add9ea08f7
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 08:37:53 -0700

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,8 @@ def print_matrix_integer(matrix=[[]]):
 
     #goes thru every sub-list in matrix
     for i in range(len(matrix)):
-        #j goes thru every element in that matrix sub-list i
-        for idx, j in matrix[i]:
+        for idx, j in enumerate(matrix[i]):
             if idx + 1 != len(matrix[i]):
                 print("{:d} ".format(j), end='')
             else:
-                #prints every element of the sub-list without going to a new line
                 print("{:d}".format(j))
