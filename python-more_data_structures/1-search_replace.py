1e9dfec7eab863d116bcde056f5e62f7062f098f
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 17:58:26 -0700

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    for idx, item in enumerate(my_list):
-        if item == search:
-            my_list[idx] = replace
-            new_list.append(my_list[idx])
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list.append(replace)
+        else:
+            new_list.append(my_list[i]
     return new_list
