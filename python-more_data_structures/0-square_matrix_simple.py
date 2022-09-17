433b3c5a6a2e8e62e64610c2604c5507691d887c
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 17:07:40 -0700

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     for i in range(len(matrix)):
-        for idx, j in (matrix[i]):
-            matrix[i][j] = matrix[i][j] * matrix[i][j]
+        for j in (matrix[i]):
+            j = j * j
     return matrix
 
