2cf6dbba79984a33f22c43d2db2758fb0c8f32fa
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-16 17:20:42 -0700

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    new_matrix = [[]]
     for i in range(len(matrix)):
+        new_list = []
         for j in (matrix[i]):
             j = j * j
-    return matrix
-
+            new_list.append(j)
+        new_matrix.append(new_list)
+    return new_matrix
