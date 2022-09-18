6c851fd6761ef81f14a63c58ccaf449c32e945e0
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-18 11:24:00 -0700

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    s1 = set(set_1)
-    s2 = set(set_2)
-    set_3 = []
-
-    if s1 and s2:
-        return set_3.append(s1 and s2)
+    return set_1 & set_2
