b30f0cd2e7d729cb860e9c29282e021f0694b78d
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-23 11:59:40 -0700

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -27,7 +27,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(0, size):
-                for j in range(0, size):
+            for i in range(0, self.__size):
+                for j in range(0, self.__size):
                     print('#')
             print()
