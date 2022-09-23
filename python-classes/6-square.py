78c3b99d944a000404350e107d97a8ec5c975bb5
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-23 13:23:57 -0700

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -20,7 +20,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @property
     def position(self):
         return self.__position
