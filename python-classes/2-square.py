d704e22b5173f528d572ac62087a7b32cd307d04
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-23 07:35:46 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,8 +6,8 @@ class Square:
     """Represents a square"""
 
     def __init__(self, size=0):
-        self.__size = size
         if size < 0:
             raise ValueError("size must be >= 0")
         elif size is not int:
             raise TypeError("size must be an integer")
+        self.__size = size
