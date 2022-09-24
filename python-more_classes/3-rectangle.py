1d92a9e5d5fb4d8b3c5a833d02f0b8890be97c54
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-23 20:55:23 -0700

    3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ''
         else:
-            for i in range(0, self.__height):
-                for j in range(0, self.__width - 1):
+            for i in range(0, self.__height - 1):
+                for j in range(0, self.__width):
                     print('#', end='')
                 print()
             return ('#' * self.__width)
