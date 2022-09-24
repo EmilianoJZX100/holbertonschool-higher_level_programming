56d11fdabc9ce2511bc2cfc65ea308b58066f29a
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-23 22:28:02 -0700

    7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ''
         else:
-            for i in range(0, self.__height - 1):
+            for i in range(0, self.__height):
                 for j in range(0, self.__width):
                     print('#', end='')
                 print()
