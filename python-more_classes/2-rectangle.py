30e9c99af09972620fcbe3ce75c787ec9c0cae42
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-23 19:35:58 -0700

    2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -31,8 +31,10 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+    
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
