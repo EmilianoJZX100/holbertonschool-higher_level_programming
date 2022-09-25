5e162ea4c83cf860fe7e96a2f827fae0804abe62
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-24 18:12:19 -0700

    8-rectangle.py

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -68,7 +68,6 @@ class Rectangle:
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle"""
 
-
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
