ad3cff025a6f043456b3545d24bc62b1e689bbd0
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-26 16:10:16 -0700

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+    """inherits from list"""
+
+
+class MyList:
+    """prints the list sorted"""
+
+
+    def print_sorted(self):
+        print(sorted(self))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
+
+
