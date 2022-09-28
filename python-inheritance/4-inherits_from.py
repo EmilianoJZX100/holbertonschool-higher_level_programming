e0912c3457b818cbb801fde4ff7701eceee71a61
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-28 13:10:53 -0700

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,9 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns True if the obj is an instance of a class that inherited"""
-    if not isinstance(a, int):
-        return False
-    elif not isinstance(a, list):
-        return False
-    else:
-        return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
