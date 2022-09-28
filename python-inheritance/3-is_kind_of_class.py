0d4014ed86cad3bcfc2e3983b3ebe0344f4ba887
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-28 12:54:58 -0700

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""instance of a class or of a subclass"""
+
+
+def is_kind_of_class(obj, a_class):
+    """true if its an instance of class or subclass, otherwise false"""
+    return is_kind_of_class(a, a_class)
