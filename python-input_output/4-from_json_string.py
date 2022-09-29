24f41f1c7546f94554b9ab0a2ae20b7afcb505cf
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-28 19:12:45 -0700

    4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """from json to string"""
+import json
 
 
 def from_json_string(my_str):
     """returns an object represented by a json string"""
 
-    import json
     return json.load(my_str)
