8cd5cdc351721ffb20f083232f0ed2bf8a65eaf9
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-28 18:28:21 -0700

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""reads a text file"""
+
+
 def read_file(filename=""):
+    """reads in  utf8"""
+
     with open('my_file_0', encoding='utf-8') as f:
         read_data = f.read()
     print(f"{read_data}", end="")
