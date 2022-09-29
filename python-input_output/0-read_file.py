9c9096f15020ad35c1d2df6ab16449f270332fa3
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-28 18:26:39 -0700

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open('my_file_0', encoding='utf-8') as m:
-        read_data = m.read()
+    with open('my_file_0', encoding='utf-8') as f:
+        read_data = f.read()
+    print(f"{read_data}", end="")
