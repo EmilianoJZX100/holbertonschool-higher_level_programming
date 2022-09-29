bf492231b14277f7f40efaa32d527b4ab20a92f7
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-28 19:33:03 -0700

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" adds all args to a list"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == '__main__':
+    json_list = []
+    fielname = 'add_item.json'
+    try:
+        json_list = load_from_json_file(filename)
+    except Exception:
+        pass
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            json_list.append(arg)
+    save_to_json_file(json_list, filename)
+
+
