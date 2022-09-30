88e5036e63c4700b2a3a135fc255c14f2cf52baf
EmilianoJZX100 <5083@holbertonschool.com>
2022-09-30 11:45:00 -0700

    trying things

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""divides all elements of a matrix"""
+
+
+def matrix_reloaded(matrix, div):
+    """function"""
+    if div is not int or float:
+        raise TypeError("div must be a number")
+    if div = 0:
+        raise ZeroDivisionError("division by zero")
