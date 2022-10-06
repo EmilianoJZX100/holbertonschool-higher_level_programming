94b3d2a5d1efea638e20aeb0ddc3a2045800ddd0
EmilianoJZX100 <5083@holbertonstudents.com>
2022-10-06 14:54:37 -0700

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -62,7 +62,7 @@ class Rectangle(Base):
     def height(self, height):
         """height setter"""
         if type(height) is not int:
-            raise TypeError(f"{height} must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = height
