cf23b4583f14b0d2a59549834b4a5219ca910058
EmilianoJZX100 <5083@holbertonstudents.com>
2022-10-06 11:33:55 -0700

    models

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""base class"""
+
+
+
+class Base:
+    """new class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if type(id) != int:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
