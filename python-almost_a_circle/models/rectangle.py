05e0187b181eef32f09043544e0c899bb499ce9a
EmilianoJZX100 <5083@holbertonstudents.com>
2022-10-06 14:04:38 -0700

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,26 +9,30 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
 
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
     @property
     def width(self):
+        self.__width = width
         return self.__width
 
     @property
     def height(self):
+        self.__height = height
         return self.__height
 
     @property
     def x(self):
+        self.__x = x
         return self.__x
 
     @property
     def y(self):
+        self.__y = y
         return self.__y
 
     @width.setter
