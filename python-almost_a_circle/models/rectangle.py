3adf676f820520b97806c7728feb713863521123
EmilianoJZX100 <5083@holbertonstudents.com>
2022-10-06 12:41:05 -0700

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,43 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        type_error("width", value)
+        wh("width", value)
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        type_error("height", value)
+        wh("height", value)
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        type_error("x", value)
+        wh("x", value)
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        type_error("y", value)
+        wh("y", value)
+        self.__y = value
