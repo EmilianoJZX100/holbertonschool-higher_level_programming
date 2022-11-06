2f9bc07ef4c06b463461737adb77a3675f430efd
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-06 17:39:04 -0300

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,5 @@
 -- creates a table in the database 
 
-DROP TABLE IF EXISTS second_table;
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
@@ -10,7 +9,3 @@ INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
 INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
-INSERT INTO second_table (id, name, score) VALUES (11, "A", 12);
-INSERT INTO second_table (id, name, score) VALUES (12, "B", 12);
-INSERT INTO second_table (id, name, score) VALUES (13, "C", 12);
-INSERT INTO second_table (id, name, score) VALUES (14, "D", 12);
