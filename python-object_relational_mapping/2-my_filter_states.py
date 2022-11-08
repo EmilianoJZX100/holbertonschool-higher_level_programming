2ef4bff2863a21609dd1c9af2b49d17c7b6432a5
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-08 20:11:34 -0300

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        )
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY states.id".format(argv[4]))
+    result = c.fetchall()
+
+    for row in result:
+        if row[1] == 'argv[4]':
+            print(row)
