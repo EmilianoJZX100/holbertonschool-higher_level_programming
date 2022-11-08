b8e0969fbcaf2880173bf50b1ad7ec5ec1736f4a
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-08 17:01:59 -0300

    0-select_states.py

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+
+import mysqldb
+from sys import argv
+
+if __name__ == "__main__":
+#create connection
+db= mysqldb.connect(
+    host="localhost",
+    port=3306,
+    user=argv[1],
+    password=argv[2],
+    database=argv[3]
+    )
+
+mycursor= db.cursor()
+query = "SELECT * FROM states ORDER BY states.id ASC"
+mycursor.execute(query)
+result = mycursor.fetchall()
+print(result)
+for record in result:
+    print(record)
