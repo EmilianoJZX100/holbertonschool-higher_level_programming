fca80c87c576cbbbc31a30248a30922b6da1e79b
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-08 18:41:00 -0300

    1-filter_states.py pycodestyle fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N from the database"""
 import MySQLdb
 from sys import argv
 
@@ -13,9 +13,9 @@ if __name__ == "__main__":
         database=argv[3]
         )
 
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = 'N%' ORDER BY states.id ASC")
-    result = cursor.fetchall()
+    cu = db.cursor()
+    cu.execute("SELECT * FROM states WHERE name = 'N%' ORDER BY states.id ASC")
+    result = cu.fetchall()
 
     for row in result:
         print(row)
