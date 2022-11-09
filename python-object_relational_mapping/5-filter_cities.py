a5a228bcdbad975f451bb3e269209c9b583d0b6d
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-09 19:37:02 -0300

    5-filter_cities.py

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        )
+
+    c = db.cursor()
+    c.execute("SELECT cities.name FROM cities\
+    JOIN states ON cities.state_id = states.id AND states.name = %s ORDER BY cities.id ASC", (argv[4], ))
+    result = c.fetchall()
+
+    for row in result:
+        if row[1] == argv[4]:
+            print(row)
