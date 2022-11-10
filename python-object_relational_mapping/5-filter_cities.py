d71d4bce3197608268a206bcd9f5e68cc651ef31
EmilianoJZX100 <5083@holbertonschool.com>
2022-11-10 15:45:35 -0300

    pycodestyle fix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,10 +14,9 @@ if __name__ == "__main__":
         )
 
     c = db.cursor()
-    c.execute('''"SELECT cities.name 
-    FROM cities JOIN states 
-    ON cities.state_id = states.id 
-    AND states.name = %s ORDER BY cities.id ASC", (argv[4], )''')
+    c.execute("SELECT cities.name FROM cities \
+        JOIN states ON cities.state_id = states.id \
+            AND states.name = %s ORDER BY cities.id ASC", (argv[4], ))
     result = c.fetchall()
 
     for row, city in enumerate(result):
